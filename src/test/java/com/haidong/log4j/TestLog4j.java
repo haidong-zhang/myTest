fe256b80e3a806a44766a23af877ab461819b884
Don Zhang <don_zhang@mtelnet.com>
2018-05-21 10:51:27 +0800

    增加测试log4j的方法

--- src/test/java/com/haidong/log4j/TestLog4j.java
+++ src/test/java/com/haidong/log4j/TestLog4j.java
@@ -1,11 +1,17 @@
 package com.haidong.log4j;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class TestLog4j {
-	
+	private static Logger logger = Logger.getLogger(TestLog4j.class);
 	@Test
 	public void Test1 () {
-		
+		logger.error("----------------------");
+		logger.debug("debug");
+		logger.info("info");
+		logger.warn("warn");
+		logger.error("error");
+		logger.fatal("fatal");
 	}
 }
